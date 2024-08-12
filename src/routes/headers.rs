0aa82d849d83d509b5e05a9536a51a18e8814e1d
clawyq <clintonlaw96@gmail.com>
2024-08-13 02:02:36 +0800

    oopsies new files

---
+++ src/routes/headers.rs
@@ -0,0 +1,9 @@
+use hyper::HeaderMap;
+
+pub async fn headers(headers: HeaderMap) -> String {
+    let val = headers.get("User-Agent").unwrap();
+    match val.to_str() {
+        Ok(dog) => dog.to_owned(),
+        Err(e) => format!("what the dog doin: {e}")
+    }
+}
---
+++ src/routes/mid_mes.rs
@@ -0,0 +1,26 @@
+use axum::{extract::Request, middleware::Next, response::Response, Extension};
+use hyper::StatusCode;
+use super::SharedData;
+
+pub async fn mid_mes(Extension(shared_data): Extension<SharedData>) -> String {
+    shared_data.shared_msg
+}
+
+pub async fn mid_zhng(Extension(zhngd): Extension<String>) -> String {
+    zhngd
+}
+
+pub async fn zhng_header(mut request: Request, next: Next) -> Result<Response, StatusCode> {
+    let headers = request.headers();
+    let msg = headers
+        .get("tc-message")
+        .ok_or_else(|| {
+            println!("cannot find la sia");
+            StatusCode::NOT_FOUND
+        })?
+        .to_str().unwrap().to_owned();
+    request
+        .extensions_mut()
+        .insert(msg + " from middleware");
+    Ok(next.run(request).await)
+}
